94f70ae859bf7af750f61bae285f642b4ee980bb
Alex Birch <Birch-san@users.noreply.github.com>
2024-03-11 15:51:53 +0000

    delete more unused stuff

--- script/play.py
+++ script/play.py
@@ -1,14 +1,13 @@
 from contextlib import nullcontext
 from diffusers import UNet2DConditionModel, AutoencoderKL
-from diffusers.models.vae import DecoderOutput
+from diffusers.models.autoencoders.vae import DecoderOutput
 from transformers import CLIPPreTrainedModel, CLIPTextModel, CLIPTextModelWithProjection, CLIPTokenizer, BatchEncoding
 from transformers.modeling_outputs import BaseModelOutputWithPooling
 from transformers.models.clip.modeling_clip import CLIPTextModelOutput
 import torch
 from torch import BoolTensor, FloatTensor, LongTensor, Generator, inference_mode, cat, randn, tensor, stack
 from torch.nn.functional import pad
-from torch.backends.cuda import sdp_kernel
-from typing import List, Union, Optional, Callable, Dict, Any, Literal
+from typing import List, Union, Optional, Callable, Dict, Any
 from logging import getLogger, Logger
 from k_diffusion.sampling import get_sigmas_karras, sample_dpmpp_2m
 from os import makedirs, listdir
@@ -25,26 +24,17 @@ from src.denoisers.denoiser_factory import DenoiserFactory, DenoiserFactoryFacto
 from src.denoisers.dispatch_denoiser import DispatchDenoiser, IdentifiedDenoiser
 from src.denoisers.eps_denoiser import EPSDenoiser
 from src.denoisers.cfg_denoiser import CFGDenoiser
-from src.denoisers.nocfg_denoiser import NoCFGDenoiser
 from src.device import DeviceType, get_device_type
 from src.schedules import KarrasScheduleParams, KarrasScheduleTemplate, get_template_schedule
-# from src.diffusers_schedules import get_diffusers_euler_discrete_schedule
-from src.schedule_params import get_alphas, get_alphas_cumprod, get_betas, quantize_to#, get_sigmas
+from src.schedule_params import get_alphas, get_alphas_cumprod, get_betas, quantize_to
 from src.latents_shape import LatentsShape
 from src.added_cond import CondKwargs
 from src.dimensions import Dimensions
 from src.time_ids import get_time_ids, get_time_ids_aesthetic
 from src.device_ctx import to_device
 from src.rgb_to_pil import rgb_to_pil
-from src.attn.apply_flash_attn_processor import apply_flash_attn_processor
-from src.attn.flash_attn_processor import FlashAttnProcessor
-from src.refined_exp_solver import sample_refined_exp_s
 from src.latents_to_pils import LatentsToBCHW, LatentsToPils, make_latents_to_bchw, make_latents_to_pils
 from src.log_intermediates import LogIntermediatesFactory, LogIntermediates, make_log_intermediates_factory
-from src.latent_interposer.comfy_latent_interposer import Interposer
-from src.attn.null_attn_processor import NullAttnProcessor
-from src.attn.natten_attn_processor import NattenAttnProcessor
-from src.attn.qkv_fusion import fuse_vae_qkv
 
 logger: Logger = getLogger(__file__)
 
@@ -60,12 +50,8 @@ sampling_dtype = torch.float32
 # if you're on a Mac: don't bother with this; VRAM and RAM are the same thing.
 swap_models = False
 
-use_wdxl = False
-
 stability_model_name = 'stabilityai/stable-diffusion-xl-base-0.9'
-wdxl_model_name = 'Birchlabs/waifu-diffusion-xl-unofficial'
-default_model_name = stability_model_name
-base_unet_model_name = wdxl_model_name if use_wdxl else stability_model_name
+default_model_name = base_unet_model_name = stability_model_name
 
 use_refiner = False
 unets: List[UNet2DConditionModel] = [UNet2DConditionModel.from_pretrained(
@@ -81,23 +67,6 @@ unets: List[UNet2DConditionModel] = [UNet2DConditionModel.from_pretrained(
 ]
 base_unet: UNet2DConditionModel = unets[0]
 refiner_unet: Optional[UNet2DConditionModel] = unets[1] if use_refiner else None
-
-use_xformers_attn = False
-if use_xformers_attn:
-  from xformers.ops import MemoryEfficientAttentionFlashAttentionOp
-  for unet in unets:
-    unet.enable_xformers_memory_efficient_attention(attention_op=MemoryEfficientAttentionFlashAttentionOp)
-
-use_flash_attn = False
-if use_flash_attn:
-  for unet in unets:
-    processor = FlashAttnProcessor()
-    unet.set_attn_processor(processor)
-
-use_flash_attn_qkv_packed = True
-if use_flash_attn_qkv_packed:
-  for unet in unets:
-    apply_flash_attn_processor(unet)
 
 compile = False
 if compile:
@@ -132,84 +101,33 @@ vit_big_g: CLIPTextModelWithProjection = CLIPTextModelWithProjection.from_pretra
 
 text_encoders: List[CLIPPreTrainedModel] = [vit_l, vit_big_g]
 
-interpose_latents_to_sd1 = False
-if interpose_latents_to_sd1:
-  from safetensors.torch import load_file
-  from huggingface_hub import hf_hub_download
-  interposer = Interposer()
-  interposer.eval()
-  interposer_weights = str(hf_hub_download(
-    repo_id='city96/SD-Latent-Interposer',
-    filename='xl-to-v1_interposer-v3.1.safetensors')
-  )
-  interposer.load_state_dict(load_file(interposer_weights))
-  interposer.to(torch.float16)
+use_ollin_vae = True
+vae_kwargs: Dict[str, Any] = {
+  'torch_dtype': torch.float16,
+} if use_ollin_vae else {
+  'variant': 'fp16',
+  'subfolder': 'vae',
+  # decoder gets NaN result in float16.
+  'torch_dtype': torch.bfloat16 if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else torch.float32,
+}
+vae_name = 'madebyollin/sdxl-vae-fp16-fix' if use_ollin_vae else default_model_name
 
-  vae_name = 'stabilityai/sd-vae-ft-mse'
-  vae_kwargs: Dict[str, Any] = {
-    'torch_dtype': torch.float16
-  }
-else:
-  use_ollin_vae = True
-  vae_kwargs: Dict[str, Any] = {
-    'torch_dtype': torch.float16,
-  } if use_ollin_vae else {
-    'variant': 'fp16',
-    'subfolder': 'vae',
-    # decoder gets NaN result in float16.
-    'torch_dtype': torch.bfloat16 if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else torch.float32,
-  }
-  vae_name = 'madebyollin/sdxl-vae-fp16-fix' if use_ollin_vae else default_model_name
+vae: AutoencoderKL = AutoencoderKL.from_pretrained(
+  vae_name,
+  use_safetensors=True,
+  **vae_kwargs,
+)
+vae.enable_slicing()
+# this script won't be using the encoder
+del vae.encoder
+vae_resample_factor: int = 1 << (len(vae.config.block_out_channels) - 1)
+vae_scaling_factor: float = vae.config.scaling_factor
+vae_dtype = vae.dtype
 
-use_consistency_decoder = False
-if use_consistency_decoder:
-  assert interpose_latents_to_sd1, "consistency decoder can only decode SD1 latents, so we'll need the interposer too"
-  assert swap_models, "you're gonna need model-swapping if you wanna fit this thing in memory"
-  # pip install git+https://github.com/openai/consistencydecoder.git
-  from consistencydecoder import ConsistencyDecoder
-  cdecoder = ConsistencyDecoder(device="cuda:0") # Model size: 2.49 GB
-  vae_scaling_factor=0.18215
-  vae_resample_factor=8
-  vae_dtype = torch.float32
-else:
-  vae: AutoencoderKL = AutoencoderKL.from_pretrained(
-    vae_name,
-    use_safetensors=True,
-    **vae_kwargs,
-  )
-  vae.enable_slicing()
-  vae_attn_impl: Literal['natten', 'null', 'original'] = 'natten'
-  match(vae_attn_impl):
-    case 'natten':
-      fuse_vae_qkv(vae)
-      # NATTEN seems to output identical output to global self-attention at kernel size 17
-      # even kernel size 3 looks good (not identical, but very close).
-      # I haven't checked what's the smallest kernel size that can look identical.
-      # should save you loads of memory and compute
-      # (neighbourhood attention costs do not exhibit quadratic scaling with sequence length)
-      vae.set_attn_processor(NattenAttnProcessor(kernel_size=17))
-    case 'null':
-      for attn in [*vae.encoder.mid_block.attentions, *vae.decoder.mid_block.attentions]:
-        # you won't be needing these
-        del attn.to_q, attn.to_k
-      # doesn't mix information between tokens via QK similarity. just projects every token by V and O weights.
-      # looks alright, but is by no means identical to global self-attn.
-      vae.set_attn_processor(NullAttnProcessor())
-    case 'original':
-      # leave it as global self-attention
-      pass
-    case _:
-      raise ValueError('you are spicier than I anticipated')
-  # this script won't be using the encoder
-  del vae.encoder
-  vae_resample_factor: int = 1 << (len(vae.config.block_out_channels) - 1)
-  vae_scaling_factor: float = vae.config.scaling_factor
-  vae_dtype = vae.dtype
-
-  # TODO: code-share with how we decode images in the non-intermediates case
-  latents_to_bchw: LatentsToBCHW = make_latents_to_bchw(vae)
-  latents_to_pils: LatentsToPils = make_latents_to_pils(latents_to_bchw)
-  make_log_intermediates: LogIntermediatesFactory = make_log_intermediates_factory(latents_to_pils)
+# TODO: code-share with how we decode images in the non-intermediates case
+latents_to_bchw: LatentsToBCHW = make_latents_to_bchw(vae)
+latents_to_pils: LatentsToPils = make_latents_to_pils(latents_to_bchw)
+make_log_intermediates: LogIntermediatesFactory = make_log_intermediates_factory(latents_to_pils)
 log_intermediates_enabled = False
 
 def round_down(num: int, divisor: int) -> int:
@@ -230,15 +148,14 @@ height_lt: int = height_px // vae_resample_factor
 # and crop(0,0) (i.e. a well-framed image) make sense.
 # TODO: should non-square images condition on orig=targ=(height_px, width_px),
 #       or should they use orig=targ=(1024, 1024)?
-original_size = Dimensions(height_px*4, width_px*4) if use_wdxl else Dimensions(height_px, width_px)
+original_size = Dimensions(height_px, width_px)
 target_size = Dimensions(height_px, width_px)
 crop_coords_top_left = Dimensions(0, 0)
 aesthetic_score = 6.
 negative_aesthetic_score = 2.5
 
-cfg_scale = 12. if use_wdxl else 5.
-cfg_until: Optional[float] = None # 1.1
-refiner_cfg_scale = 5. if use_wdxl else cfg_scale
+cfg_scale = 5.
+refiner_cfg_scale = cfg_scale
 # https://arxiv.org/abs/2305.08891
 # Common Diffusion Noise Schedules and Sample Steps are Flawed
 # 3.4. Rescale Classifier-Free Guidance
@@ -248,25 +165,12 @@ cfg_rescale = 0.
 # how was SDXL trained? on zero uncond, or empty string uncond? how about for the refiner?
 # https://twitter.com/s_alt_acc/status/1683627077315227648
 # https://twitter.com/Birchlabs/status/1683949307450228741
-# diffusers default is to enable this for base UNet, disable for refiner.
-# it'd be a little fiddly in this script to make base and refiner separately configurable.
-# fortunately we can afford to get it wrong for refiner; CFG doesn't do much below sigma 1.1
-# https://birchlabs.co.uk/machine-learning#when-to-skip-cfg-for-speedup
 force_zeros_for_empty_prompt = True
 
-if use_wdxl:
-  uncond_prompt: str = 'lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name'
-else:
-  uncond_prompt: Optional[str] = None if force_zeros_for_empty_prompt else ''
+uncond_prompt: Optional[str] = None if force_zeros_for_empty_prompt else ''
 
 negative_prompt: Optional[str] = uncond_prompt
-# prompt: str = 'astronaut meditating under waterfall, in swimming shorts'
-# prompt: str = '90s anime sketch, girl wearing serafuku walking home, masterpiece, dramatic, wind'
-# prompt: str = 'photo of astronaut meditating under waterfall, in swimming shorts, breathtaking, 4k, dslr, cinematic, global illumination'
-# prompt: str = 'ancient text, book, highly intricate, lonely, breathtaking, 4k, dslr, cinematic, global illumination'
-# prompt: str = 'the dragon attacks at night, masterpiece, dramatic, highly detailed, high dynamic range'
-# prompt: str = 'photo of absolute power, grand celestial woman'
-prompt: str = 'photo of powerful, elegant woman'
+prompt: str = 'photo of absolute power, grand celestial woman'
 prompts: List[str] = [
   *([] if negative_prompt is None or cfg_scale == 1. else [negative_prompt]),
   prompt,
@@ -298,7 +202,7 @@ for tokenizer_ix, (tokenized, tokenizer, text_encoder) in enumerate(zip(tokenize
   attention_mask: BoolTensor = tensor(tokenized.attention_mask, device=device, dtype=torch.bool)
   embedding_masks.append(attention_mask)
 
-  with to_device(text_encoder, device), inference_mode(), sdp_kernel(enable_math=False) if torch.cuda.is_available() else nullcontext():
+  with to_device(text_encoder, device), inference_mode():
     assert isinstance(text_encoder, CLIPTextModel) or isinstance(text_encoder, CLIPTextModelWithProjection), f'text_encoder of type "{type(text_encoder)}" was unexpectedly neither a CLIPTextModel nor a CLIPTextModelWithProjection'
     encoder_out: Union[BaseModelOutputWithPooling, CLIPTextModelOutput] = text_encoder(
       input_ids=input_ids,
@@ -373,56 +277,7 @@ def make_cfg_denoiser(
     guidance_rescale=cfg_rescale,
     cross_attention_mask=cross_attention_mask,
   )
-def make_no_cfg_denoiser(
-  delegate: Denoiser,
-  cross_attention_conds: FloatTensor,
-  added_cond_kwargs: CondKwargs,
-  # unused param, used to satisfy factory interface
-  cfg_scale: float,
-  cross_attention_mask: Optional[BoolTensor] = None,
-) -> CFGDenoiser:
-  return NoCFGDenoiser(
-    denoiser=delegate,
-    cross_attention_conds=cross_attention_conds,
-    added_cond_kwargs=added_cond_kwargs,
-    cross_attention_mask=cross_attention_mask,
-  )
-if cfg_scale > 1:
-  if cfg_until is None:
-    denoiser_factory_factory: DenoiserFactoryFactory[CFGDenoiser] = lambda delegate: partial(make_cfg_denoiser, delegate)
-  else:
-    def make_cfg_until_denoiser(
-      delegate: Denoiser,
-      cross_attention_conds: FloatTensor,
-      added_cond_kwargs: CondKwargs,
-      cfg_scale: float,
-      cross_attention_mask: Optional[BoolTensor] = None,
-    ) -> DispatchDenoiser:
-      cfg_denoiser = CFGDenoiser(
-        denoiser=delegate,
-        cross_attention_conds=cross_attention_conds,
-        added_cond_kwargs=added_cond_kwargs,
-        cfg_scale=cfg_scale,
-        guidance_rescale=cfg_rescale,
-        cross_attention_mask=cross_attention_mask,
-      )
-      no_cfg_denoiser = NoCFGDenoiser(
-        denoiser=delegate,
-        cross_attention_conds=cross_attention_conds.chunk(2)[1],
-        added_cond_kwargs=CondKwargs(
-          text_embeds=added_cond_kwargs['text_embeds'].chunk(2)[1],
-          time_ids=added_cond_kwargs['time_ids'].chunk(2)[1],
-        ),
-        cross_attention_mask=None if cross_attention_mask is None else cross_attention_mask.chunk(2)[1],
-      )
-      cfg_id = IdentifiedDenoiser('cfg', cfg_denoiser)
-      nocfg_id = IdentifiedDenoiser('nocfg', no_cfg_denoiser)
-      def pick_delegate(sigma: float) -> IdentifiedDenoiser:
-        return cfg_id if sigma > cfg_until else nocfg_id
-      return DispatchDenoiser(pick_delegate=pick_delegate)
-    denoiser_factory_factory: DenoiserFactoryFactory[DispatchDenoiser] = lambda delegate: partial(make_cfg_until_denoiser, delegate)
-else:
-  denoiser_factory_factory: DenoiserFactoryFactory[NoCFGDenoiser] = lambda delegate: partial(make_no_cfg_denoiser, delegate)
+denoiser_factory_factory: DenoiserFactoryFactory[CFGDenoiser] = lambda delegate: partial(make_cfg_denoiser, delegate)
 
 base_denoiser_factory: DenoiserFactory[Denoiser] = denoiser_factory_factory(base_unet_k_wrapped)
 refiner_denoiser_factory: Optional[DenoiserFactory[Denoiser]] = denoiser_factory_factory(refiner_unet_k_wrapped) if use_refiner else None
@@ -444,17 +299,6 @@ sigmas: FloatTensor = get_sigmas_karras(
   rho=rho,
   device=device,
 ).to(sampling_dtype)
-# sigmas: FloatTensor = get_sigmas_exponential(
-#   n=25,
-#   sigma_min=base_unet_k_wrapped.sigma_min,
-#   sigma_max=base_unet_k_wrapped.sigma_max,
-#   device=device,
-# ).to(sampling_dtype)
-
-# here's how to use non-Karras sigmas
-# steps=25
-# sigmas = base_unet_k_wrapped.get_sigmas(steps)
-# sigma_max, sigma_min = sigmas[0], sigmas[-2]
 
 print(f"sigmas (unquantized):\n{', '.join(['%.4f' % s.item() for s in sigmas])}")
 sigmas_quantized = pad(quantize_to(sigmas[:-1], base_unet_k_wrapped.sigmas), pad=(0, 1))
@@ -499,20 +343,15 @@ generator = Generator(device='cpu')
 
 max_batch_size = 8
 
-# start_seed = 42
 start_seed = 29
-# start_seed = 45
 sample_count = 1
 seeds = range(start_seed, start_seed+sample_count)
 
 if not swap_models:
   base_unet.to(device)
-  if not use_consistency_decoder:
-    vae.to(device)
+  vae.to(device)
   if use_refiner:
     refiner_unet.to(device)
-  if interpose_latents_to_sd1:
-    interposer.to(device)
 
 img_provenance: str = 'refined' if use_refiner else 'base'
 
@@ -591,28 +430,13 @@ for batch_ix, batch_seeds in enumerate(batched(seeds, max_batch_size)):
   else:
     denoiser: Denoiser = base_denoiser
 
-  # only usable with ancestral samplers. gives more stable convergence (so you can change step count and get a similar image).
-  # for reproducibility: you'll want to use a batch-of-1, to have a seed-per-sample rather than per-batch.
-  # noise_sampler = BrownianTreeNoiseSampler(
-  #   latents,
-  #   sigma_min=sigma_min,
-  #   sigma_max=start_sigma,
-  #   # there's no requirement that the noise sampler's seed be coupled to the init noise seed;
-  #   # I'm just re-using it because it's a convenient arbitrary number
-  #   seed=seeds[0],
-  # )
-
   tic: float = perf_counter()
 
-  with inference_mode(), to_device(base_unet, device) if swap_models else nullcontext(), sdp_kernel(enable_math=False) if torch.cuda.is_available() else nullcontext():
+  with inference_mode(), to_device(base_unet, device) if swap_models else nullcontext():
     denoised_latents: FloatTensor = sample_dpmpp_2m(
-    # denoised_latents: FloatTensor = sample_refined_exp_s(
-    # denoised_latents: FloatTensor = sample_dpmpp_3m_sde(
       denoiser,
       latents,
       sigmas,
-      # eta=1.,
-      # noise_sampler=noise_sampler, # you can only pass noise sampler to ancestral samplers such as sample_dpmpp_2m_sde
       callback=callback,
     ).to(vae_dtype)
 
@@ -620,25 +444,16 @@ for batch_ix, batch_seeds in enumerate(batched(seeds, max_batch_size)):
     batch_secs: float = perf_counter() - tic
     per_sample_secs: float = batch_secs/batch_size
     torch.cuda.synchronize()
-    print(f"batch-of-{batch_size} took {batch_secs:.2f}s with cfg_until={(0 if cfg_until is None else cfg_until):.2f} and use_refiner={use_refiner}. that's {per_sample_secs:.2f}s/sample. Measurement includes UNet(s) only.")
+    print(f"batch-of-{batch_size} took {batch_secs:.2f}s with use_refiner={use_refiner}. that's {per_sample_secs:.2f}s/sample. Measurement includes UNet(s) only.")
 
   if swap_models:
     refiner_unet.cpu()
 
   denoised_latents = denoised_latents / vae_scaling_factor
 
-  if interpose_latents_to_sd1:
-    with inference_mode(), to_device(interposer, device) if swap_models else nullcontext():
-      denoised_latents = interposer.forward(denoised_latents.to(interposer.head_short.weight.dtype))
-
-  if use_consistency_decoder:
-    with inference_mode():
-      sample: FloatTensor = cdecoder(denoised_latents.to(vae_dtype))
-  else:
-    # cannot use flash attn because VAE decoder's self-attn has head_dim 512
-    with inference_mode(), to_device(vae, device) if swap_models else nullcontext():#, sdp_kernel(enable_math=False) if torch.cuda.is_available() else nullcontext():
-      decoder_out: DecoderOutput = vae.decode(denoised_latents.to(vae_dtype))
-    sample: FloatTensor = decoder_out.sample
+  with inference_mode(), to_device(vae, device) if swap_models else nullcontext():
+    decoder_out: DecoderOutput = vae.decode(denoised_latents.to(vae_dtype))
+  sample: FloatTensor = decoder_out.sample
 
   sample: FloatTensor = sample.div(2).add(.5).clamp(0,1)
   for ix, (decoded, out_stem) in enumerate(zip(sample, out_stems)):
